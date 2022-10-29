8b6f43866e7f8cce0ba33c151516364b6248f867
ishmaSenaweera <86140992+ishmaSenaweera@users.noreply.github.com>
2022-10-29 14:06:35 +0530

    Sidebar Added

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import Router from "./components/routers/Router";
 import { AuthContextProvider } from "./components/userManagement/context/UserContext";
+import "./App.css";
+import Sidebar from "./Components/Sidebar";
 
 axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
     <AuthContextProvider>
+      <Sidebar />
       <Router />
     </AuthContextProvider>
   );
---
+++ web_frontend/src/Components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { SidebarData } from "./SidebarData";
+import "./Styles/SidebarStyles.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="brand">
+        <span className="title1">
+            EyePax
+        </span>
+        <span className="title2">
+            CONSTRUCTIONS
+        </span>
+      </div>
+      <div className="links">
+        <ul className="sidebar-list">
+          {SidebarData.map((value, index) => {
+            return (
+              <li
+                key={index}
+                onClick={() => (window.location.pathname = value.path)}
+                className="sidebar-item"
+                id={window.location.pathname === value.path ? "selected" : ""}
+              >
+                <div id="icon">{value.icon}</div>
+                <div id="title">{value.title}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
